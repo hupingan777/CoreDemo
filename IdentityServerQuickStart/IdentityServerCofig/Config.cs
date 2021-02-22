53382bd71fb5bc28f11639571b05b459d64085df
hupingan777 <1203074898@qq.com>
2021-02-22 10:04:12 +0800

    IdentityServerQuickStart

---
+++ IdentityServerQuickStart/IdentityServerCofig/Config.cs
@@ -0,0 +1,54 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerQuickStart.IdentityServerCofig
+{
+    public static class Config
+    {
+        /// <summary>
+        /// api资源
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1","My API")
+            };
+        }
+
+        /// <summary>
+        /// 客户端应用，这里采用客户端凭证模式
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "mvc client",
+                    AllowedGrantTypes = new string[] { GrantType.ClientCredentials},
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                },
+                new Client
+                {
+                    ClientId = "vue client",
+                    AllowedGrantTypes = new string[] { GrantType.ResourceOwnerPassword},
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
